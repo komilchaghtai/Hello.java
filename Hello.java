2cbf2e8e5cf05522c4c9cedfe0102e5f0424a6ec
komilchaghtai <69979470+komilchaghtai@users.noreply.github.com>
2020-08-20 11:49:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************
+*Hello,java.
+**********************
+*Komil Chaghtai
+*August 20, 2020
+*CMSC 255 Section 001
+***********************************/
+public class Hello {
+	public static void main(String[]args) {
+
+	System.out.println("<Komil Chaghtai>");
+	System.out.println("<Hello.java.>");
+	System.out.println("<Lab 1>");
+	System.out.println("<August 20 2020>");
+	System.out.println("<CMSC 255 001>");
+	System.out.println();
+	
+	}
+}
